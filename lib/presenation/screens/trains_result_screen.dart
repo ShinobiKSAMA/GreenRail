6e5f0bd7f5fb7fb1ac4b596fc9a1d3b7799a8180
ShinobiKSAMA <adityagmhatre@gmail.com>
2024-07-01 22:47:05 +0530

    DateTab

--- lib/presenation/screens/trains_result_screen.dart
+++ lib/presenation/screens/trains_result_screen.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:greenrail/presenation/widgets/common/opt_in_tile.dart';
 
 class TrainsResultScreen extends StatefulWidget {
@@ -9,21 +11,65 @@ class TrainsResultScreen extends StatefulWidget {
 }
 
 class _TrainsResultScreenState extends State<TrainsResultScreen> {
+  int selected = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text("PNVL to KTE"),
-      ),
-      child: SingleChildScrollView(
-        // padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(height: 76),
-            OptInTile(),
-          ],
-        ),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            centerTitle: true,
+            title: const Text("PNVL to PUNE"),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(48),
+              child: SizedBox(
+                height: 48,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Transform.rotate(
+                      angle: -pi / 2,
+                      child: const Text("JUL"),
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        itemBuilder: (_, i) => ChoiceChip(
+                          label: Text("${i + 1} ${getWeekday(i)}"),
+                          selected: i == selected,
+                          onSelected: (value) => setState(() => selected = i),
+                        ),
+                        separatorBuilder: (_, __) => const SizedBox(width: 4),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: OptInTile(rounded: false),
+          ),
+        ],
       ),
     );
+  }
+
+  String getWeekday(int i) {
+    final int weekday = DateTime.now().copyWith(day: i + 1).weekday;
+    return switch (weekday) {
+      1 => "Mon",
+      2 => "Tue",
+      3 => "Wed",
+      4 => "Thu",
+      5 => "Fri",
+      6 => "Sat",
+      7 => "Sun",
+      _ => ""
+    };
   }
 }
--- lib/presenation/widgets/common/opt_in_tile.dart
+++ lib/presenation/widgets/common/opt_in_tile.dart
@@ -3,20 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:greenrail/core/extensions/context_extension.dart';
 
 class OptInTile extends StatelessWidget {
-  const OptInTile({
-    super.key,
-  });
+  const OptInTile({super.key, this.rounded = true});
+
+  final bool rounded;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: CheckboxListTile(
-        shape: RoundedRectangleBorder(
-            side: BorderSide(color: context.colorScheme.outline, width: 1),
-            borderRadius: const BorderRadius.all(Radius.circular(12))),
+        shape: rounded
+            ? RoundedRectangleBorder(
+                side: BorderSide(color: context.colorScheme.outline, width: 1),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+              )
+            : null,
         visualDensity: VisualDensity.compact,
         secondary: const Icon(CupertinoIcons.lock_shield_fill, size: 45),
         title: const Text("Opt-in for Free Cancellation"),
+        tileColor: context.colorScheme.primaryContainer.withOpacity(0.5),
         subtitle: const Text(
           "\u{20B9}0 cancellation fee and Instant refund",
           overflow: TextOverflow.ellipsis,
